890d20f0746ba2d2de5c1ff4d7c2dc95219ec82e
Prakash choudhary <85573801+prakash-choudhary2014@users.noreply.github.com>
2023-03-23 00:45:54 +0530

    Update NewsPage.jsx

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -1,42 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import NewsCard from './NewsCard';
-// import '../styles/Home.module.css'
 
-// const NewsPage = () => {
-//   const [news, setNews] = useState([]);
 
-//   useEffect(() => {
-//     const apiKey = '01cc5300bf964991bc9f5082e72fa8bd';
-//     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
-
-//     axios.get(apiUrl)
-//       .then(response => {
-//         setNews(response.data.articles);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }, []);
-
-//   return (
-//     <div className="news-page">
-//       <h1 className="page-title">Latest News</h1>
-//       <div className="news-list">
-//         {news.map((article) => (
-//           <NewsCard
-//             key={article.title}
-//             title={article.title}
-//             image={article.urlToImage}
-//             description={article.description}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NewsPage;
 
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
@@ -46,18 +9,17 @@
 //   const [articles, setArticles] = useState([]);
 //   const [searchQuery, setSearchQuery] = useState('');
 //   const [isLoading, setIsLoading] = useState(false);
+//   const [category, setCategory] = useState('general');
 
 //   const API_KEY = '01cc5300bf964991bc9f5082e72fa8bd';
 
-
-
-//   // Fetch article data from news api using axios 
+  
 //   useEffect(() => {
 //     setIsLoading(true);
 //     const fetchArticles = async () => {
 //       try {
 //         const response = await axios.get(
-//           `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+//           `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
 //         );
 //         setArticles(response.data.articles);
 //         setIsLoading(false);
@@ -67,8 +29,10 @@
 //       }
 //     };
 //     fetchArticles();
-//   }, []);
+//   }, [category]);
 
+
+//   //for searching by keywords
 //   const handleSearch = async (event) => {
 //     event.preventDefault();
 //     setIsLoading(true);
@@ -84,22 +48,46 @@
 //     }
 //   };
 
-//   return (
+  
+// //for searching by category
+//   const handleCategoryChange = (event) => {
+//     setCategory(event.target.value);
+//   };
 
-//     // Search bar
+//   return (
+//     // Search bar and filter bar
+    
 //     <div className="news-container">
+//       <div className='cat-key'>
 //       <form onSubmit={handleSearch}>
-//         <div className='search-container'>
-//         <input
-//           type="text"
-//           placeholder="Search by keywords..."
-//           value={searchQuery}
-//           onChange={(event) => setSearchQuery(event.target.value)}
-//         />
-//         <button type="submit" className='search-button'>Search</button>
+//         <div className="search-container">
+//           <input
+//             type="text"
+//             placeholder="Search by keywords..."
+//             value={searchQuery}
+//             onChange={(event) => setSearchQuery(event.target.value)}
+//           />
+//           <button type="submit" className="search-button">
+//             Search
+//           </button>
 //         </div>
 //       </form>
 
+//       <div className="filter-container">
+//         <label htmlFor="category">Filter by category:</label>
+//         <select id="category" value={category} onChange={handleCategoryChange}>
+//           <option value="general">General</option>
+//           <option value="business">Business</option>
+//           <option value="entertainment">Entertainment</option>
+//           <option value="health">Health</option>
+//           <option value="science">Science</option>
+//           <option value="sports">Sports</option>
+//           <option value="technology">Technology</option>
+//         </select>
+//       </div>
+
+//   </div>
+      
 //       {isLoading ? (
 //         <p>Loading...</p>
 //       ) : (
@@ -121,8 +109,6 @@
 
 // export default NewsPage;
 
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NewsCard from './NewsCard';
@@ -133,15 +119,20 @@ const NewsPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [category, setCategory] = useState('general');
 
-  const API_KEY = '01cc5300bf964991bc9f5082e72fa8bd';
+  const API_KEY = 'xY4eld4d3OtYJxdkWH7ZHFemcg5RRUdz5f8B2xLpxlU';
+  const BASE_URL = 'https://api.newscatcherapi.com/v2/';
 
-  
   useEffect(() => {
     setIsLoading(true);
     const fetchArticles = async () => {
       try {
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+          `${BASE_URL}search?q=${category}&lang=en&sort_by=relevancy&page=1`,
+          {
+            headers: {
+              'x-api-key': API_KEY,
+            },
+          }
         );
         setArticles(response.data.articles);
         setIsLoading(false);
@@ -160,7 +151,12 @@ const NewsPage = () => {
     setIsLoading(true);
     try {
       const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${API_KEY}`
+        `${BASE_URL}search?q=${searchQuery}&lang=en&sort_by=relevancy&page=1`,
+        {
+          headers: {
+            'x-api-key': API_KEY,
+          },
+        }
       );
       setArticles(response.data.articles);
       setIsLoading(false);
@@ -170,8 +166,7 @@ const NewsPage = () => {
     }
   };
 
-  
-//for searching by category
+  //for searching by category
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   };
@@ -202,15 +197,12 @@ const NewsPage = () => {
           <option value="business">Business</option>
           <option value="entertainment">Entertainment</option>
           <option value="health">Health</option>
-          <option value="science">Science</option>
-          <option value="sports">Sports</option>
           <option value="technology">Technology</option>
-        </select>
-      </div>
+          </select>
+          </div>
+          </div>
 
-  </div>
-      
-      {isLoading ? (
+          {isLoading ? (
         <p>Loading...</p>
       ) : (
         <div className="news-list">
@@ -218,9 +210,9 @@ const NewsPage = () => {
             <NewsCard
               key={index}
               title={article.title}
-              description={article.description}
-              image={article.urlToImage}
-              url={article.url}
+              description={article.summary}
+              image={article.media}
+              url={article.link}
             />
           ))}
         </div>
@@ -230,5 +222,4 @@ const NewsPage = () => {
 };
 
 export default NewsPage;
-
 
